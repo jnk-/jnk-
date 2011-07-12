0a9a0f4d0fc2d523607b68a999a52a6b2ac2d139
jnk <junkyismaker@gmail.com>
2011-07-13 03:44:30 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jnkapp::Application.config.secret_token = 'c2f99a901470bbc8eee19024cb8d90b14d9410751a8f01d033c63c35eede700813916347be6c9156d2d39b6c9c34ef391a5f9d28fed3f8b77c7bf9dea3279ab5'
